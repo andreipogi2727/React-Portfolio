96c30415c56df61687e5797dad7624a96c631e4c
Andrei Haboc <habocandrei0627@gmail.com>
2024-12-04 18:32:12 -0500

    Removed screen size useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,6 @@ import Project from "./components/Projects/Project.tsx";
 import Contact from "./components/Contact/Contact.tsx";
 
 function App() {
-  const [screenSize, setScreenSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      const newWidth = window.innerWidth;
-      const newHeight = window.innerHeight;
-
-      // Update state with new sizes
-      setScreenSize({
-        width: newWidth,
-        height: newHeight,
-      });
-
-      // Log the screen sizes
-      console.log(
-        `Width: ${newWidth}px, Height: ${newHeight}px Screen Size: ${screenSize}`
-      );
-    };
-
-    // Add event listener for resize
-    window.addEventListener("resize", handleResize);
-
-    // Clean up the event listener
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
-
   return (
     <div className="app">
       <Navbar />
